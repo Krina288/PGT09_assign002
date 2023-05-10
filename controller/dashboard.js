7940f9c8b9868118f1124f459941966a0032b91c
krina <krinampatel28@gmail.com>
2023-05-10 13:30:31 +0100

    change server setup

--- controller/dashboard.js
+++ controller/dashboard.js
@@ -1,10 +1,15 @@
+// const imgprofile = require('../assets/profile-user.png')
 let arrPostList = [
     { 'id': 1, 'title': 'Test Event1', 'message': 'Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.' },
     { 'id': 2, 'title': 'Test Event2', 'message': 'Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.' },
     { 'id': 3, 'title': 'Test Event3', 'message': 'Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.' },
     { 'id': 4, 'title': 'Test Event4', 'message': 'Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.' },
-    { 'id': 5, 'title': 'Test Event5', 'message': 'Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.' }]
+    { 'id': 5, 'title': 'Test Event5', 'message': 'Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.' }
+]
 
+// $(document).ready(function onPressAddPost () {
+//         alert('Post is created successfully.')
+// })
 function onPressPost(index) {
     alert('testing' + index)
 }
@@ -13,12 +18,8 @@ function onPressSignOut() {
     window.location.href = '../Login/login.html';
 }
 
-function onPressAddPost() {
-    alert('Post is created successfully.')
-}
-
 function onPressPostDetail() {
-    window.location.href = '../ViewPost/viewPost.html';
+    window.location.href = '../view/viewPost.html';
 }
 
 function onPressEditPost() {
--- server.js
+++ server.js
@@ -1,38 +1,90 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
     const urlPath = req.url
-    console.log('request url: ', urlPath);
-    if (urlPath === '/' && req.method === 'POST') {
-        // if (urlPath === '/login.html') {
-        //     fs.readFile('../PGT09_assign002/components/Dashboard/dashboard.html', 'utf8', (err, content) => {
-        //     // fs.readFile('index.html', 'utf8', (err, content) => {
-        //         if (err) {
-        //             console.log('errrr', err);
-        //             res.writeHead(500, { 'Content-Type': 'text/plain' });
-        //             res.end('Internal Server Error');
-        //         } else {
-        //             res.writeHead(200, { 'Content-Type': 'text/html' });
-        //             res.write(content);  
-        //             res.end();
-        //         }
-        //     });
-        // } else if (urlPath === '/dashboard.html') {
-            fs.readFile('../PGT09_assign002/components/Dashboard/dashboard.html', 'utf8', (err, content) => {
-                // fs.readFile('index.html', 'utf8', (err, content) => {
-                if (err) {
-                    console.log('errrr', err);
-                    res.writeHead(500, { 'Content-Type': 'text/plain' });
-                    res.end('Internal Server Error');
-                } else {
-                    res.writeHead(200, { 'Content-Type': 'text/html' });
-                    res.write(content);
-                    res.end();
+    console.log('request url: ', urlPath, req.method);
+    const file = __dirname
+    if (urlPath === '/' || urlPath === '/login.html') {
+        const filePath = path.join(file.concat('/view'), '/login.html');
+        console.log('filepath, ', filePath);
+        fs.readFile(filePath, 'utf8', (err, content) => {
+            if (err) {
+                res.writeHead(500);
+                res.end('Error serving login page');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(content);
+            }
+        });
+    } else if (urlPath === '/dashboard.html') {
+        // fs.readFile('../PGT09_assign002/components/Dashboard/dashboard.html', 'utf8', (err, content) => {
+        console.log('fileee: ', file);
+        const filePath = path.join(file.concat('/view'), '/dashboard.html');
+        console.log('filepath, ', filePath);
+        fs.readFile(filePath, (err, content) => {
+            if (err) {
+                res.writeHead(500);
+                res.end('Error serving dashboard page');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(content);
+            }
+        });
+    } else if (urlPath === '/viewPost.html') {
+        // fs.readFile('../PGT09_assign002/components/Dashboard/dashboard.html', 'utf8', (err, content) => {
+        console.log('fileee: ', file);
+        const filePath = path.join(file.concat('/view'), '/viewPost.html');
+        console.log('filepath, ', filePath);
+        fs.readFile(filePath, (err, content) => {
+            if (err) {
+                res.writeHead(500);
+                res.end('Error serving viewpost page');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(content);
+            }
+        });
+    } else if (urlPath === '/dashboard.js') {
+        // fs.readFile('../PGT09_assign002/components/Dashboard/dashboard.html', 'utf8', (err, content) => {
+        console.log('fileee: ', file);
+        const filePath = path.join(__dirname, '/dashboard.js');
+        console.log('filepath, ', filePath);
+        fs.readFile(filePath, (err, content) => {
+            if (err) {
+                res.writeHead(500);
+                res.end('Error serving dashboard.js page');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/javascript' });
+                res.end(content);
+            }
+        });
+    } else if (urlPath.startsWith === '/assests') {
+        //create seprate folder for view and controller ---- tomorrow
+        const filePath = path.join(__dirname, '/assests');
+        console.log('new filepath: ', filePath);
+        fs.readFile(filePath, (err, content) => {
+            if (err) {
+                res.writeHead(500);
+                res.end('Error serving file');
+            } else {
+                const ext = path.extname(filePath);
+                let contentType = 'image/jpeg';
+
+                if (ext === '.png') {
+                    contentType = 'image/png';
+                } else if (ext === '.gif') {
+                    contentType = 'image/gif';
                 }
-            });
-        // }
-    } else {
+
+                res.writeHead(200, { 'Content-Type': contentType });
+                res.write(content);
+                res.end();
+            }
+        });
+    }
+    else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('Page not found');
     }
